b50720dc94f519ef74c00d9ba599720997b9d9df
walkerreynolds16 <walkerreynolds16@gmail.com>
2016-04-12 11:27:48 -0400

    File /C:/Users/Walker/workspace/CPSC 250/src/Assignment4/Robot.java changed at Tue Apr 12 11:27:48 EDT 2016

--- src/Assignment4/Robot.java
+++ src/Assignment4/Robot.java
@@ -9,97 +9,110 @@ import java.util.Scanner;
 
 public class Robot {
 
-	public static void readInstructions(String inputFileName,
-			String outputFileName) {
+    public static void readInstructions(String inputFileName,
+            String outputFileName) {
 
-		Scanner scan = null;
-		PrintWriter writer = null;
+        Scanner scan = null;
+        PrintWriter writer = null;
 
-		try {
+        try {
 
-			File inputFile = new File(inputFileName);
+            File inputFile = new File(inputFileName);
 
-			File outputFile = new File(outputFileName);
+            File outputFile = new File(outputFileName);
 
-			scan = new Scanner(inputFile);
+            scan = new Scanner(inputFile);
 
-			writer = new PrintWriter(outputFile);
+            writer = new PrintWriter(outputFile);
 
-			while (scan.hasNextLine()) {//while the input file has a next line
+            while (scan.hasNextLine()) {// while the input file has a next line
 
-				String line = scan.nextLine();//splits the line
+                String line = scan.nextLine();// splits the line
 
-				Scanner split = new Scanner(line);
+                Scanner split = new Scanner(line);
 
-				try {
+                try {
 
-					boolean isTerminated = false;
+                    boolean isTerminated = false;
 
-					int gridRows = split.nextInt() - 1;//initial grid size y
-					int gridCols = split.nextInt() - 1;//initial grid size x
+                    int gridRows = split.nextInt() - 1;// initial grid size y
+                    int gridCols = split.nextInt() - 1;// initial grid size x
 
-					int robotY = split.nextInt();//initial robot y
-					int robotX = split.nextInt();//initial robot x
+                    int robotY = split.nextInt();// initial robot y
+                    int robotX = split.nextInt();// initial robot x
 
-					if (gridCols < 1 || gridRows < 1) {//checks if the robot is outside the grid initially, if so throw an exception
-						throw new InvalidRobotInstructionException();
-					}
+                    if (gridCols < 1 || gridRows < 1) {// checks if the robot is
+                                                       // outside the grid
+                                                       // initially, if so throw
+                                                       // an exception
+                        throw new InvalidRobotInstructionException();
+                    }
 
-					while (split.hasNext()) {//while the line has a next command
+                    while (split.hasNext()) {// while the line has a next
+                                             // command
 
-						String direction = split.next();
-						int numOfMoves = split.nextInt();
+                        String direction = split.next();
+                        int numOfMoves = split.nextInt();
 
-						if (direction.equalsIgnoreCase("E")) {
-							robotX += numOfMoves;
+                        if (direction.equalsIgnoreCase("E")) {
+                            robotX += numOfMoves;
 
-						} else if (direction.equalsIgnoreCase("W")) {
-							robotX -= numOfMoves;
+                        }
+                        else if (direction.equalsIgnoreCase("W")) {
+                            robotX -= numOfMoves;
 
-						} else if (direction.equalsIgnoreCase("N")) {
-							robotY -= numOfMoves;
+                        }
+                        else if (direction.equalsIgnoreCase("N")) {
+                            robotY -= numOfMoves;
 
-						} else if (direction.equalsIgnoreCase("S")) {
-							robotY += numOfMoves;
-						} else {
-							throw new InvalidRobotInstructionException();
-						}
+                        }
+                        else if (direction.equalsIgnoreCase("S")) {
+                            robotY += numOfMoves;
+                        }
+                        else {
+                            throw new InvalidRobotInstructionException();
+                        }
 
-						//checks if the robot is outside the grid
-						if (robotX > gridCols || robotX < 0
-								|| robotY > gridRows || robotY < 0) {
-							isTerminated = true;
-						}
-					}
+                        // checks if the robot is outside the grid
+                        if (robotX > gridCols || robotX < 0|| robotY > gridRows || robotY < 0) {
+                            isTerminated = true;
+                        }
+                    }
 
-					if (isTerminated) {
-						writer.println("terminated");
-					} else {
-						writer.println(robotY + " " + robotX);
-					}
+                    if (isTerminated) {
+                        writer.println("terminated");
+                    }
+                    else {
+                        writer.println(robotY + " " + robotX);
+                    }
 
-				} catch (InputMismatchException e) {
-					throw new InvalidRobotInstructionException();
+                }
+                catch (InputMismatchException e) {
+                    throw new InvalidRobotInstructionException();
 
-				} catch (NoSuchElementException e) {
-					throw new InvalidRobotInstructionException();
+                }
+                catch (NoSuchElementException e) {
+                    throw new InvalidRobotInstructionException();
 
-				} finally {
-					split.close();
-				}
-			}
+                }
+                finally {
+                    split.close();
+                }
+            }
 
-		} catch (FileNotFoundException e) {
-			System.out.println("Input file not found.");
-		} finally {
-			if (writer != null) {
-				writer.close();
-			}
-			if (scan != null) {
-				scan.close();
-			}
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Input file not found.");
+        }
+        finally {
+            if (writer != null) {
+                writer.close();
+            }
+            if (scan != null) {
+                scan.close();
+            }
 
-		}
-	}
+        }
+    }
 
 }
